02bccc563a446a0606081d5af28326080bc85a1a
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-01-11 10:23:54 +0530

    used useMemo

--- Practice/05-Counter-UseMemo/src/App.jsx
+++ Practice/05-Counter-UseMemo/src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useMemo } from "react";
+import { useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0);
+  const [count,setCount] = useState(0);
+
+  let sum = useMemo(() => {
+    let x = 0;
+    for (let i = 1; i <= number; i++) {
+      x += i;
+    }
+    return x;
+  },[number])
+
 
   return (
     <>
-      
+      <div className="container p-5">
+        <div className="row justify-content-center">
+          <div className="col-6">
+            <div className="input-box">
+              <label htmlFor="number" className="form-label">
+                Number
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="number"
+                onChange={(e) => setNumber(e.target.value)}
+              />
+            </div>
+            <div className="sum">{sum}</div>
+            <div className="counter">
+              <button onClick={() => setCount(count + 1)}>{count}</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
